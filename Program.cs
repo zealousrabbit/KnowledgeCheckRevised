ac4d414da59768e761f8bfb53cdc746727dfb84c
Hannah Rodgers <miss.hannah.rodgers@gmail.com>
2024-04-09 18:04:06 -0400

    added code

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿    Console.WriteLine("Please enter first number: ");
+    int first = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Please enter second number: ");
+    int second = int.Parse(Console.ReadLine());
+
+Console.WriteLine("State your operator: + - / * ");
+string symboltype = Console.ReadLine();
+
+switch (symboltype)
+{
+
+    case "+":
+            
+            Console.WriteLine($"The solution is: {first + second}");
+        break;
+
+    case "-":
+         
+            Console.WriteLine($"The solution is: {first - second}");
+        break;
+
+    case "*":
+        
+            Console.WriteLine($"The solution is: {first * second}");
+        break;
+
+    case "/":
+     
+            Console.WriteLine($"The solution is: {first / second}");
+        break;
+
+    default: 
+      Console.WriteLine("I'm sorry, I don't know what you mean.");
+      break;
+    }
+   
